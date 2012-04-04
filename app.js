7b3495eb224bf9d2e46fbecba559c8e2510ea925
Chris Olstrom <chris@olstrom.com>
2012-04-04 16:36:29 -0700

    ADDED: --help to display usage data.

--- app.js
+++ app.js
@@ -3,11 +3,11 @@
 var config = require('optimist')
 	.options('mass', {
 		alias	: 'm',
-		default	: 132.5	// in Kg
+		default	: 132.5
 	})
 	.options('height', {
 		alias	: 'h',
-		default	: 186	// in cm
+		default	: 186
 	})
 	.options('fat', {
 		alias	: 'f',
@@ -15,7 +15,7 @@ var config = require('optimist')
 	})
 	.options('gender', {
 		alias	: 'g',
-		default	: 1	// 0:F | 1:M
+		default	: 1
 	})
 	.options('age', {
 		alias	: 'a',
@@ -33,6 +33,7 @@ var config = require('optimist')
 		alias	: 'M',
 		default	: 3
 	})
+	.boolean('help')
 	.argv
 ;
 function getBMR() {
@@ -89,18 +90,30 @@ function IntakeModel() {
 	this.init();
 }
 
-var intake = new IntakeModel();
 function showPlan(meals) {
 	console.log('\t' + 'Calories: ' + Math.round(intake.calories.minimum / meals) + ' to ' + Math.round(intake.calories.maximum / meals));
 	console.log('\t' + 'Protein: ' + Math.round(intake.protein.minimum / meals) + ' grams (' + Math.round(( intake.protein.minimum * caloriesPerGram.protein ) / meals) + ' calories)' );
 	console.log('\t' + 'Carb: ' + Math.round(intake.carbs.minimum / meals) + ' to ' + Math.round(intake.carbs.maximum / meals) + ' grams' );
 	console.log('\t' + 'Fat: ' + Math.round(intake.fat.minimum / meals) + ' to ' + Math.round(intake.fat.maximum / meals) + ' grams (' + Math.round(( intake.fat.minimum * caloriesPerGram.fat ) / meals) + ' to ' + Math.round(( intake.fat.maximum * caloriesPerGram.fat ) / meals) + ' calories)');
 };
-
-console.log('Totals for Today\n');
-showPlan(1);
-
-if ( config.meals > 1 ) {
-	console.log('\nMeal Plan (each of ' + config.meals + ' meals should contain...)\n');
-	showPlan(config.meals);
+if ( config.help ) {
+	console.log('Diet Planner v2012.04.04',
+		'\tby Chris Olstrom <chris@olstrom.com>\n',
+		'\n\t-A | --activity  Activity level, from 1 (sedentary) to 5 (pro athlete), 0 to disable.',
+		'\n\t-a | --age       Age, in years.',
+		'\n\t-f | --fat       Bodyfat percentage, in decimal notation',
+		'\n\t-g | --gender    Gender, 0 for female, 1 for male.',
+		'\n\t-h | --height    Height, in centimeters.',
+		'\n\t-M | --meals     Meals per day.',
+		'\n\t-m | --mass      Mass, in kilograms.',
+		'\n\t-r | --rate      Rate of loss, in pounds per week.'
+	);
+} else {
+	var intake = new IntakeModel();
+	console.log('Totals for Today\n');
+	showPlan(1);
+	if ( config.meals > 1 ) {
+		console.log('\nMeal Plan (each of ' + config.meals + ' meals should contain...)\n');
+		showPlan(config.meals);
+	}
 }
